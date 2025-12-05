18ed9725487839389b25b12f501508c6db4ed4a7
Tu <tienvinh1611@gmail.com>
2025-12-05 15:01:21 +0700

    up date tour

--- app/controllers/AdminTourController.php
+++ app/controllers/AdminTourController.php
@@ -101,4 +101,31 @@ class AdminTourController extends Controller
 
         $this->render('admin/tours/edit', compact('tour', 'hotels', 'transports'));
     }
+      public function show()
+    {
+        $id = $_GET['id'] ?? null;
+
+        if (!$id) {
+            $this->redirect('index.php?controller=adminTour&action=index');
+        }
+
+        $tour = $this->tourModel->find($id);
+
+        $partnerModel = new Partner();
+        $hotel = $partnerModel->find($tour['hotel_id']);
+        $transport = $partnerModel->find($tour['transport_id']);
+
+        $this->render('admin/tours/show', compact('tour', 'hotel', 'transport'));
+    }
+
+
+    public function delete()
+    {
+        $id = $_GET['id'] ?? null;
+        if ($id) {
+            $this->tourModel->delete($id);
+        }
+        $this->redirect('index.php?controller=adminTour&action=index');
+    }
 }
+
